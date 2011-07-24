39189466b4b08986dd6d5a2324cfc76610b5be47
Wellington Rocha Parra <wrparra@hotmail.com>
2011-07-24 18:51:53 -0300

    [UPDATE] Updating the folder structure

--- src/DownloadAllDependenciesNow.bat
+++ src/DownloadAllDependenciesNow.bat
@@ -1,2 +1,2 @@
-"Tools\nuget.exe" install "BolaoFutebolClube.Web\packages.config" -o "Packages"
-"Tools\nuget.exe" install "BolaoFutebolClube.Tests\packages.config" -o "Packages"
+"Tools\nuget.exe" install "Presentations\BolaoFutebolClube.Web\packages.config" -o "Packages"
+"Tools\nuget.exe" install "Tests\BolaoFutebolClube.Tests\packages.config" -o "Packages"
